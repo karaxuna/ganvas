51ded44a9002e85a4f1fbcc11e3a47814f8064e5
karaxuna <kaxabernet@gmail.com>
2016-06-08 16:04:41 +0400

    Polygon added

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
-import Line from './lib/Line';
+import Polygon from './lib/Polygon';
 import Scene from './lib/Scene';
 import Point from './lib/Point';
 
-var line = new Line(
+var polygon = new Polygon(
     new Point(0, 0),
-    new Point(100, 100),
-    new Point(200, 200),
-    5
+    [new Point(100, 100), new Point(200, 200), new Point(100, 200)]
 );
 
-var line2 = new Line(
-    new Point(0, 0),
-    new Point(200, 100),
-    new Point(100, 200),
-    5
-);
-
-var scene = new Scene(document.getElementById('canvas').getContext('2d'), [line, line2]);
+var scene = new Scene(document.getElementById('canvas').getContext('2d'), [polygon]);
 scene.draw();
 
 var i = 1;
 setInterval(function () {
-    if (line.position.y > 50) {
+    if (polygon.position.y > 50) {
         i = -1;
     }
-    else if (line.position.y < 5) {
+    else if (polygon.position.y < 5) {
         i = 1;
     }
     
-    line.position.move({ x: 0, y: i });
+    polygon.position.move({ x: 0, y: i });
     scene.draw();
 }, 20);
---
+++ src/lib/Polygon.js
@@ -0,0 +1,48 @@
+import utils from './utils';
+import Figure from './Figure';
+import Point from './Point';
+
+class Polygon extends Figure {
+    constructor(position, points) {
+        super(position);
+        this.points = points;
+    }
+    
+    draw() {
+        var self = this,
+            scene = self.scene,
+            context = scene.context,
+            absPosition = self.absPosition,
+            points = self.points;
+
+        context.beginPath();
+        context.moveTo(absPosition.x + points[0].x, absPosition.y + points[0].y);
+
+        for (var i = 1; i < points.length; i++) {
+            var point = points[i];
+            context.lineTo(absPosition.x + point.x, absPosition.y + point.y);
+        }
+
+        context.closePath();
+        context.stroke();
+        return this;
+    }
+    
+    pointInside(pt) {
+        var self = this,
+            points = self.points,
+            absPosition = self.absPosition;
+
+        for (var rpointi, rpointj, c = false, i = -1, l = points.length, j = l - 1; ++i < l; j = i) {
+            rpointi = new Point(absPosition.x + points[i].x, absPosition.y + points[i].y);
+            rpointj = new Point(absPosition.x + points[j].x, absPosition.y + points[j].y);
+
+            ((rpointi.y <= pt.y && pt.y < rpointj.y) || (rpointj.y <= pt.y && pt.y < rpointi.y)) &&
+                (pt.x < (rpointj.x - rpointi.x) * (pt.y - rpointi.y) / (rpointj.y - rpointi.y) + rpointi.x) &&
+                (c = !c);
+        }
+        return c;
+    }
+}
+
+module.exports = Polygon;
